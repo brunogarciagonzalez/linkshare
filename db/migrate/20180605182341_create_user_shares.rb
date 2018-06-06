5c2172bdb9eb17f8f1584aa5b2b1bafb73694cf2
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-06 14:32:22 -0400

    added active:boolean attribute

--- db/migrate/20180605182341_create_user_shares.rb
+++ db/migrate/20180605182341_create_user_shares.rb
@@ -4,6 +4,7 @@ class CreateUserShares < ActiveRecord::Migration[5.1]
       t.integer :user_id
       t.integer :review_id
       t.integer :link_id
+      t.boolean :active, default: true
 
       t.timestamps
     end
--- db/migrate/20180605182833_create_link_tag_joins.rb
+++ db/migrate/20180605182833_create_link_tag_joins.rb
@@ -3,6 +3,7 @@ class CreateLinkTagJoins < ActiveRecord::Migration[5.1]
     create_table :link_tag_joins do |t|
       t.integer :link_id
       t.integer :tag_id
+      t.boolean :active, default: true
 
       t.timestamps
     end
