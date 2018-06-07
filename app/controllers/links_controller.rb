af127be8c1ca9a9fba64002c6425c4c42c231236
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-06 21:51:17 -0400

    links_controller#update && links_controller#destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -32,14 +32,40 @@ class LinksController < ApplicationController
   end
 
   def construct_link
+    url_from_params = strong_construct_link_params[:url]
 
+    @link = Link.new(url: url_from_params)
+
+    if @link.save
+      render json: {status: "success", action: "construct_link", link: @link}, status: 200
+    else
+      render json: {status: "failure", action: "construct_link", errors: @link.errors.full_messages}, status: 200
+    end
   end
 
   def update_link
+    link_id_from_params = strong_update_link_params[:id]
+    new_link_content_from_params = strong_update_link_params[:url]
 
+    @link = Link.find(link_id_from_params)
+
+    if @link.update(url: new_link_content_from_params)
+      render json: {status: "success", action: "update_link", link: @link}, status: 200
+    else
+      render json: {status: "failure", action: "update_link", errors: @link.errors.full_messages}, status: 200
+    end
   end
 
   def destroy_link
+    link_id_from_params = strong_destroy_link_params[:id]
+    @link = Link.find(link_id_from_params)
+
+    if @link
+      @link.destroy
+        render json: {status: "success", action: "destroy_link", link: @link}, status: 200
+    else
+      render json: {status: "failure", action: "destroy_link", details: "link not found (by id)"}, status: 200
+    end
   end
 
   private
@@ -49,6 +75,18 @@ class LinksController < ApplicationController
   end
 
   def strong_get_link_params
+    params.require(:link).permit(:id)
+  end
+
+  def strong_construct_link_params
+    params.require(:link).permit(:url)
+  end
+
+  def strong_update_link_params
+    params.require(:link).permit(:id, :url)
+  end
+
+  def strong_destroy_link_params
     params.require(:link).permit(:id)
   end
 end
