b55de8b8c586a3318f44a098e5508181f62ae335
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-06-15 15:22:57 -0400

    change @current_user to @user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
- before_action :authorized
+  before_action :authorized
 
- def current_user
-   @current_user = @current_user || authenticate_or_request_with_http_token do |jwt_token, options|
+  def user
+   @user = @user || authenticate_or_request_with_http_token do |jwt_token, options|
      begin
        decoded_token = JWT.decode(jwt_token, "secret")
 
@@ -12,17 +12,17 @@ class ApplicationController < ActionController::API
      end
 
      if decoded_token[0]["user_id"]
-       @current_user ||= User.find(decoded_token[0]["user_id"])
+       @user ||= User.find(decoded_token[0]["user_id"])
      else
      end
    end
- end
+  end
 
- def logged_in?
-   !!current_user
- end
+  def logged_in?
+   !!user
+  end
 
- def authorized
+  def authorized
    render json: {message: "Unauthorized" }, status: 401 unless logged_in?
- end
+  end
 end
