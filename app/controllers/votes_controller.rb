ad14e4fb178a5e6bdca63175b7a43c13d0eee463
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-07-09 15:54:19 -0400

    finish vote-related

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -22,6 +22,13 @@ class VotesController < ApplicationController
       # did the current user vote for this review?
       current_user_voted = false
 
+      # is current user the author of the review?
+      review_is_by_current_user = false
+
+      if @review.reviewer.id == current_user_id
+        review_is_by_current_user = true
+      end
+
       # count number of helpful
       num_helpful = 0
 
@@ -33,7 +40,7 @@ class VotesController < ApplicationController
           current_user_voted = true
         end
       end
-      render json: {status: "success", action: "get_vote", helpful_votes: num_helpful, current_user_voted: current_user_voted}, status: 200
+      render json: {status: "success", action: "get_vote", helpful_votes: num_helpful, current_user_voted: current_user_voted, review_is_by_current_user: review_is_by_current_user}, status: 200
     else
 
       # count number of helpful
@@ -44,7 +51,7 @@ class VotesController < ApplicationController
           num_helpful += 1
         end
       end
-      render json: {status: "success", action: "get_vote", helpful_votes: num_helpful, current_user_voted: false }, status: 200
+      render json: {status: "success", action: "get_vote", helpful_votes: num_helpful, current_user_voted: false, review_is_by_current_user: false }, status: 200
     end
 
 
